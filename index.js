222139e76b2ca809b8b57e0a1e89f02507a454fb
Jon Woo <jw56578@gmail.com>
2019-01-15 10:38:49 -0600

    files

---
+++ index.js
@@ -0,0 +1,7 @@
+const removeDuplicates = require("./removeDuplicates");
+const zigzagConversion = require("./zigzagConversion");
+
+module.exports  = {
+    removeDuplicates,
+    zigzagConversion
+}
---
+++ removeDuplicates.js
@@ -0,0 +1,9 @@
+
+/*
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+Given nums = [1,1,2],
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+*/
+module.exports.removeDuplicates = function(sortedArray){
+
+}
